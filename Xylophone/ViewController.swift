d37e536e40141830cceea7bf60629b06120af569
z3t4z00k <zetazook@gmail.com>
2020-10-06 00:49:02 +0530

    Added function to play corresponding sounds

--- Xylophone/ViewController.swift
+++ Xylophone/ViewController.swift
@@ -6,9 +6,35 @@
 //
 
 import UIKit
+import AVFoundation
 
 class ViewController: UIViewController {
+    
+    var player: AVAudioPlayer!
 
+    func playSound(soundName: String) {
+        let url = Bundle.main.url(forResource: soundName, withExtension: "wav")
+        player = try! AVAudioPlayer(contentsOf: url!)
+        player.play()
+    }
+    
+    @IBAction func keyAPressed(_ sender: UIButton) {
+        playSound(soundName: sender.currentTitle!)
+    }
+
+    @IBAction func keyBPressed(_ sender: UIButton) {
+    }
+    @IBAction func keyCPressed(_ sender: UIButton) {
+    }
+    @IBAction func keyDPressed(_ sender: UIButton) {
+    }
+    @IBAction func keyEPressed(_ sender: UIButton) {
+    }
+    @IBAction func keyFPressed(_ sender: UIButton) {
+    }
+    @IBAction func keyGPressed(_ sender: UIButton) {
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
